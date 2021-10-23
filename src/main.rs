2a1251fcc20d7db4f81307502b8c99daf54de1a1
mark <67511608+Marusiella@users.noreply.github.com>
2021-10-23 11:38:41 +0200

    dziala hasowanie

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::fmt::{self, Display};
 
+use sha2::Sha512;
+
 struct Block {
     index: u64,
     previus_hash: String,
@@ -28,9 +30,8 @@ impl Create_block for Block {
 }
 impl Display for Block {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "({}, {}, {}, {}, {})", self.index, self.previus_hash, format!("{:?}", self.timestamp), format!("{:?}", self.data), format!("{:?}", self.hash) )
+        write!(f,"{} {:?} {:?} {:?} {:?}", self.index, self.previus_hash, self.timestamp, self.data, self.hash)
     }
-
 }
 
 fn calculate_hash(index: u64, previus_hash: String, timestamp: std::time::Instant, data: Vec<String>) -> String {
@@ -40,7 +41,25 @@ fn calculate_hash(index: u64, previus_hash: String, timestamp: std::time::Instan
     hasher.update(before.as_bytes());
     format!("{:02x}",hasher.finalize())
 }
-
+fn calculate_hash_proof(index: u64, previus_hash: String, timestamp: std::time::Instant, data: Vec<String>, proof: String) -> String {
+    use sha2::{Sha512,Digest};
+    let mut hasher = Sha512::new();
+    let before = index.to_string().parse::<String>().unwrap() + &previus_hash + &format!("{:?}",timestamp) + &format!("{:?}", data);
+    hasher.update(before.as_bytes());
+    let steps: u128 = std::u128::MAX;
+    for x in 0..steps {
+        // let mut s = hasher.clone();
+        if format!("{:02x}",hasher.clone().finalize())[..proof.len()] == proof {
+            println!("{} sssssssssss",format!("{:02x}",hasher.clone().finalize()));
+            break;
+        } else {
+            // println!("{:02x}", hasher.clone().finalize());
+            hasher.update(x.to_string().as_bytes());
+        }
+        if x%1000000 == 0 { println!("{}",x)}
+    }
+    format!("{:02x}",hasher.finalize())
+}
 fn main() {
     // println!("{}",calculate_hash(1,"sss".to_string(),std::time::Instant::now(),vec!["dd".to_string()]))
     let first = Block {
@@ -55,4 +74,11 @@ fn main() {
     // for _ in 0..10000 {
         // println!("{}",calculate_hash(1,"sss".to_string(),std::time::Instant::now(),vec!["dd".to_string()]))
     // }
+    use sha2::{Sha512,Digest};
+    let mut hasher = Sha512::new();
+    let before = "qwerty";
+    hasher.update(before.as_bytes());
+    
+    println!("{}",format!("{:02x}",hasher.finalize()));
+    println!("{}",calculate_hash_proof(1,"sss".to_string(),std::time::Instant::now(),vec!["dd".to_string()], "babu".to_string()));
 }
